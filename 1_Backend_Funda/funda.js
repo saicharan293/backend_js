6676d99896bec05f8e5c4e89a3e40c84e1e614e4
saicharan293 <2493saicharan@gmail.com>
2025-11-06 07:49:38 +0530

    What is Node js actually and exactly

--- 1_Backend_Funda/funda.js
+++ 1_Backend_Funda/funda.js
@@ -41,3 +41,15 @@
 var arr = [1, 2, 3, 4]
 var first = arr.indexOf(5);
 console.log(first);
+
+//-------------------------------------------------------------------------------------------------
+
+// Back story: Js ni use chesi backend ni create cheyalemu, endukante Js daggara backend ni run cheyagala
+// functionalities levu, "Ryan Dahl" Js ni use chesi backend ni create cheyali anukunnadu.
+// 
+//Node js: Node js ante Js runtime environment, V8 engine and Js combo tho ne
+//  javascript server ni create/run cheyagalamu
+// Node = Js wrapper + v8 engine .... combo
+
+// Note: Node run ayyentha varaku, Js backend run avtu untadi.
+//  ( Js ni use chesi, server create chese chance istadi)
