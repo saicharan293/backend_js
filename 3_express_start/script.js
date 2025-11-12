2bd61ed4d9cf49495e3357675fa13075f63c52ab
saicharan293 <2493saicharan@gmail.com>
2025-11-12 08:10:34 +0530

    added middle ware

--- 3_express_start/script.js
+++ 3_express_start/script.js
@@ -4,6 +4,12 @@ const app = express();
 
 // express package ni use chesi routes ni create cheyachu
 
+// app.use((req, res, next)=>{next();}); idi oka middle ware
+app.use(function(req, res, next){
+    console.log("idi middle ware");
+    next();
+})
+
 app.get("/", function(req, res){
     res.send("first route and get")
 });
