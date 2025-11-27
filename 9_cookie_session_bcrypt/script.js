ee6e1ddf6e40de113cdc8f8793e53b4ccd5cf7ab
saicharan293 <2493saicharan@gmail.com>
2025-11-27 22:28:07 +0530

    bcrypt, hash, compare

--- 9_cookie_session_bcrypt/script.js
+++ 9_cookie_session_bcrypt/script.js
@@ -1,16 +1,29 @@
 const express = require('express');
-const cookieParser = require('cookie-parser');
+// const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
 const app = express();
 
-app.use(cookieParser());
-
+// app.use(cookieParser());
+//$2b$10$ln5cOP1G9hGiDrTOKoHCP.7sFaIgZlmWebUEu3WFcnof7jlA4mqBa
 app.get("/", (req, res)=>{
-    res.cookie("name", "sai");
+    bcrypt.genSalt(10, function(err, salt){
+        bcrypt.hash("na istam", 10, function(err, hash){
+            console.log(hash);
+            
+        })
+    })
     res.send("cookies start cheddam mawa");
 })
 
+app.get("/check", (req, res)=>{
+    bcrypt.compare("na istam", "$2b$10$ln5cOP1G9hGiDrTOKoHCP.7sFaIgZlmWebUEu3WFcnof7jlA4mqBa", function(err, result){
+        console.log("kathi anduko janaki: ", result);
+        res.send("kathi dorikindi");
+    })
+})
+
 app.get("/read", (req, res)=>{
-    console.log(req.cookies);
+    // console.log(req.cookies);
     res.send("read route")
 })
 
