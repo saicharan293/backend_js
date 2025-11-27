8cb80670ce0373b23d65d89190540c88e55278e3
saicharan293 <2493saicharan@gmail.com>
2025-11-27 22:40:17 +0530

    jwt sign and verify

--- 9_cookie_session_bcrypt/script.js
+++ 9_cookie_session_bcrypt/script.js
@@ -1,29 +1,38 @@
 const express = require('express');
-// const cookieParser = require('cookie-parser');
-const bcrypt = require('bcrypt');
 const app = express();
+const cookieParser = require('cookie-parser');
+// const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-// app.use(cookieParser());
+app.use(cookieParser());
 //$2b$10$ln5cOP1G9hGiDrTOKoHCP.7sFaIgZlmWebUEu3WFcnof7jlA4mqBa
 app.get("/", (req, res)=>{
-    bcrypt.genSalt(10, function(err, salt){
-        bcrypt.hash("na istam", 10, function(err, hash){
-            console.log(hash);
+    // bcrypt.genSalt(10, function(err, salt){
+    //     bcrypt.hash("na istam", 10, function(err, hash){
+    //         console.log(hash);
             
-        })
-    })
+    //     })
+    // })
+    let token = jwt.sign({email: "test@ex.com"}, "secret");
+    res.cookie("token", token);
+    console.log(token);
+    
     res.send("cookies start cheddam mawa");
 })
 
 app.get("/check", (req, res)=>{
-    bcrypt.compare("na istam", "$2b$10$ln5cOP1G9hGiDrTOKoHCP.7sFaIgZlmWebUEu3WFcnof7jlA4mqBa", function(err, result){
-        console.log("kathi anduko janaki: ", result);
-        res.send("kathi dorikindi");
-    })
+    // bcrypt.compare("na istam", "$2b$10$ln5cOP1G9hGiDrTOKoHCP.7sFaIgZlmWebUEu3WFcnof7jlA4mqBa", function(err, result){
+    //     console.log("kathi anduko janaki: ", result);
+    //     res.send("kathi dorikindi");
+    // })
+    let data = jwt.verify(req.cookies.token, "secret");
+    console.log("nayaldi: ", data);
+    res.send("nayaldi...")
+    
 })
 
 app.get("/read", (req, res)=>{
-    // console.log(req.cookies);
+    console.log(req.cookies.token);
     res.send("read route")
 })
 
