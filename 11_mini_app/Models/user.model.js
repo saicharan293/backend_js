c5f11bab6f3e9dfd359102b466850ab6d8e5ca12
saicharan293 <2493saicharan@gmail.com>
2025-12-07 20:56:56 +0530

    upto middle ware

--- 11_mini_app/Models/user.model.js
+++ 11_mini_app/Models/user.model.js
@@ -7,8 +7,14 @@ const userSchema = mongoose.Schema({
     username: String,
     name: String,
     password: String,
+    hashedPassword: String,
     age: Number,
-    email: String
+    email: String,
+    posts: [
+        {
+            type: mongoose.Schema.Types.ObjectId, ref: "post"
+        }
+    ]
 });
 
 module.exports = mongoose.model('user', userSchema);
--- 11_mini_app/server.js
+++ 11_mini_app/server.js
@@ -3,12 +3,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const userModel = require('./Models/user.model');
+let bcrypt = require('bcrypt');
+let jwt = require('jsonwebtoken');
 
 
 app.set("view engine", 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname,'public')));
+// app.use(express.static(path.join(__dirname,'public')));
 
 app.use(cookieParser());
 
@@ -16,5 +18,50 @@ app.get("/",(req, res)=>{
     res.render("index")
 });
 
+app.post("/create", async(req, res)=>{
+    let {email, password, username, name, age} = req.body;
+    let checkExisting = await userModel.findOne({email});
+    if(checkExisting) return res.status(409).send('User already registered with this email');
+    let hashed = await bcrypt.hash(password, 10);
+    let newUser = await userModel.create({ email, username, name, password, hashedPassword: hashed, age});
+    let token = jwt.sign({email: newUser.email, userid: newUser._id}, "secret");
+    res.cookie("token", token);
+    res.send("registered");
+})
+
+app.get("/login",(req, res)=>{
+    res.render("login")
+});
+app.post("/login", async(req, res)=>{
+    let {email, password} = req.body;
+    let checkExisting = await userModel.findOne({email});
+    if(!checkExisting) return res.status(401).send('Something went wrong');
+    let response = await bcrypt.compare(password, checkExisting.hashedPassword);
+    if(!response) res.status(401).send("Unauthorized login");
+    let token = jwt.sign({email, userid: checkExisting._id}, "secret");
+    res.cookie("token", token);
+    res.status(200).send("Succesfully logged in");
+})
+
+app.get('/logout', async(req, res)=>{
+    res.cookie("token" ,"");
+    res.redirect("/login");
+})
+
+app.get("/profile", isLoggedIn, (req, res)=>{
+    res.send("profile page");
+})
+
+function isLoggedIn(req, res, next){
+    let token = req.cookies.token;
+    if(!token) res.redirect("/login");
+    try {
+        let data = jwt.verify(req.cookies.token, "secret");
+        req.user = data;
+        return next();
+    } catch (error) {
+        return res.redirect("/login");
+    }
+}
 
 app.listen(3000,()=>console.log("program start aindi"));
